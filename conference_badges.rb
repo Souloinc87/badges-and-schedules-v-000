d36479b8c40c61f631994731e8136bf49eedc451
Marcus  = Soulo <souloinc87@gmail.com>
2018-03-21 00:42:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  names = []
-  array.each {|x| names << x}
-end 
-badge_maker(names)
-end 
+  array.each do |names|
+    badge_maker(names)
+ end
+end
