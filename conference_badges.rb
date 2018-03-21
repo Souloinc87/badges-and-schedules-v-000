bf87e8685d36f66ba6164ebab3ac5a0afaabd6e9
Marcus  = Soulo <souloinc87@gmail.com>
2018-03-21 00:55:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,12 @@ def batch_badge_creator(array)
   array.each {|x| arr << badge_maker(x)}
   arr
 end
+
+def assign_rooms(array)
+  counter = 1 
+  array.map do |list|
+    puts "Hello #{list}! You'll be assigned to room #{counter}!"
+    counter += 1
+  end 
+end 
+  
